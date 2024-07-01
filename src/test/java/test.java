78cf1a4fb2c5f94390e42e8bd5073cfc51e7d760
Minseob So <pksomin@gmail.com>
2024-07-01 10:03:55 +0900

    jdbc 연결
    test Test로 바꿀려했는데 충돌나서 나중에 JdbcConnectionTest 로 바꾸거나 하겠습니다.
    
    while문 -> try-catch로 바꿨습니다.
    나머지 변경 사항 번경했습니다.

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,17 +1,11 @@
+import lombok.extern.slf4j.Slf4j;
 import java.sql.*;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class test {
-    private static final Logger logger = LoggerFactory.getLogger(test.class);
-    private static final int MAX_RETRY_COUNT = 3;
-
     @Test
     public void testJDBCConnection() {
-        Connection con = null;
-        int retryCount = 0;
-
         String url = "jdbc:mysql://localhost:3306/quiz_db"; // 서버 주소
         String userName = "root"; //  접속자 id
         String password = "0000"; // 접속자 pw
@@ -20,34 +14,14 @@ public class test {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            logger.error("JDBC 드라이버를 로드하는데에 문제 발생", e);
-            return;
+            log.error("JDBC 드라이버를 로드하는데에 문제 발생", e);
         }
 
         // 접속
-        while (retryCount < MAX_RETRY_COUNT) {
-            try {
-                con = DriverManager.getConnection(url, userName, password);
-                logger.info("연결 완료!");
-                break;
-            } catch (SQLException e) {
-                retryCount++;
-                logger.error("연결 오류 발생. 재시도 중...", e);
-                if (retryCount == MAX_RETRY_COUNT) {
-                    logger.error("최대 재시도 횟수 초과. 연결에 실패했습니다.");
-                    return;
-                }
-            }
-        }
-
-        // 접속 종료
-        try {
-            if (con != null) {
-                con.close();
-                logger.info("연결이 정상적으로 종료되었습니다.");
-            }
+        try (Connection con = DriverManager.getConnection(url, userName, password)) {
+            log.info("연결 완료!");
         } catch (SQLException e) {
-            logger.error("연결 종료 중 오류 발생", e);
+            log.error("연결 오류 발생.", e);
         }
     }
 }
