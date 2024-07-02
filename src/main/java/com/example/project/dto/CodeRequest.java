31e3bfbd57cde5668510e45373ecbddf37ae8ac3
usr <rlarlgus0206@naver.com>
2024-07-02 23:12:14 +0900

    update : Lombok

--- src/main/java/com/example/project/dto/CodeRequest.java
+++ src/main/java/com/example/project/dto/CodeRequest.java
@@ -1,43 +1,13 @@
 //클라이언트로부터 코드 실행 요청을 받을 DTO
 package com.example.project.dto;
 
+import lombok.Data;
+
+@Data
 public class CodeRequest {
     private String language;
     private String code;
     private String input;
     private String expectedOutput;
 
-
-    // Getter and Setter methods
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getInput() {
-        return input;
-    }
-
-    public void setInput(String input) {
-        this.input = input;
-    }
-
-    public String getExpectedOutput() {
-        return expectedOutput;
-    }
-
-    public void setExpectedOutput(String expectedOutput) {
-        this.expectedOutput = expectedOutput;
-    }
 }
--- src/main/java/com/example/project/dto/CodeResult.java
+++ src/main/java/com/example/project/dto/CodeResult.java
@@ -1,33 +1,11 @@
 //코드 실행 결과를 나타내는 DTO
 package com.example.project.dto;
 
+import lombok.Data;
+
+@Data
 public class CodeResult {
     private Long id;
     private String output;
     private boolean isCorrect;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getOutput() {
-        return output;
-    }
-
-    public void setOutput(String output) {
-        this.output = output;
-    }
-
-    public boolean isCorrect() {
-        return isCorrect;
-    }
-
-    public void setCorrect(boolean isCorrect) {
-        this.isCorrect = isCorrect;
-    }
 }
