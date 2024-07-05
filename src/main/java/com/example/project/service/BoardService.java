0e010adf16ee531f36e47c1767477063a4ef9010
usr <rlarlgus0206@naver.com>
2024-07-06 00:24:11 +0900

    update : readAll과 @Transactional 애노테이션 추가

--- src/main/java/com/example/project/service/BoardService.java
+++ src/main/java/com/example/project/service/BoardService.java
@@ -1,13 +1,16 @@
 package com.example.project.service;
 
 import com.example.project.dto.board.BoardResponse;
-import com.example.project.dto.board.request.BoardDeleteRequest;
-import com.example.project.dto.board.request.BoardReadRequest;
-import com.example.project.dto.board.request.BoardRegisterRequest;
-import com.example.project.dto.board.request.BoardUpdateRequest;
+import com.example.project.dto.board.request.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -19,6 +22,7 @@ public class BoardService {
     * BoardRegisterRequest를 인자로 받아 와서 데아터베이스에 정보를 넣고 생성한다.
     * 생성 후 생성된 테이블 정보를 BoardResponse에 답아서 보낸다
     * */
+    @Transactional
     public BoardResponse register(BoardRegisterRequest request){
 
         return new BoardResponse();
@@ -32,6 +36,7 @@ public class BoardService {
     * 수정은 request에 있는 title과 context를 PATCH하는 방식으로 한다
     * 마지막에 board에 대한 정보를 BoardResponse에 담아서 리턴한다.
     * */
+    @Transactional
     public BoardResponse update(BoardUpdateRequest request){
 
         return new BoardResponse();
@@ -43,11 +48,20 @@ public class BoardService {
     * BoardReadRequest를 인자로 받는다
     * 인자로 받은 후 boardId를 가지고 데이터베이스에서 정보를 찾아서 board에 대한 정보를 BoardResponse에 담아서 리턴한다
     * */
+    @Transactional
     public BoardResponse read(BoardReadRequest request){
 
         return new BoardResponse();
     }
 
+    /*
+    * TODO - readAll
+    * BoardReadAllRequest를 받아서 그 SolutionID와 같은 모든 테이블을 가져올 수 있도록 만든다.
+    * */
+    @Transactional
+    public List<BoardResponse> readAll(BoardReadAllRequest request){
+        return new LinkedList<>();
+    }
 
     /*
     * TODO - delete
@@ -55,6 +69,7 @@ public class BoardService {
     * 만약 boardId의 writerId와 userId가 같다면 삭제를 허가한다.
     * 리턴값이 없다.
     * */
+    @Transactional
     public void delete(BoardDeleteRequest request){
 
     }
