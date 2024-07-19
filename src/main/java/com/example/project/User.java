bc88c3d12527a8e569209dcc408d1493726f8003
김주연 <jooyeon5204@gmail.com>
2024-07-19 21:44:03 +0900

    feat : JPA 엔티티를 사용할 수 있게하고 객체 생성을 더 편하게 할 수 있도록 변경

--- src/main/java/com/example/project/User.java
+++ src/main/java/com/example/project/User.java
@@ -1,11 +1,18 @@
 package com.example.project;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity // JPA 엔티티 사용하기 위해
+@NoArgsConstructor // 기본 생성자 추가
+@AllArgsConstructor // 모든 필드를 포함해 생성자 추가
+
 public class User {
     @Id
-    private final String id;
-    private final String password;
+    private String id;
+    private String password;
 }
