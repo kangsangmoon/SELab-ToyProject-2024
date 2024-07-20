772d04ee7d2a81124681fcbf3eb270a23e84e2e9
kyungmin <alsdl7950@naver.com>
2024-07-20 17:41:11 +0900

    update: HomeController fix:메인, 로그인, 회원가입 페이지

--- src/main/java/com/example/project/controller/HomeController.java
+++ src/main/java/com/example/project/controller/HomeController.java
@@ -3,12 +3,13 @@ package com.example.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/main")
-    public String mainPage(){
+    public String mainPage() {
 
         //jsp
         //thymeleaf
@@ -16,5 +17,17 @@ public class HomeController {
         //model.addAttribute("name","kim");
 
         return "main_page";
+    }
+
+    @GetMapping("/signin_page")
+    public String signinPage() {
+
+        return "signin_page";
+    }
+
+    @GetMapping("/signup_page")
+    public String signupPage() {
+
+        return "signup_page";
     }
 }
