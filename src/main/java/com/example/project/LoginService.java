9edbac49d7b8d6120e5c64f3b6cf8238bff3f637
김주연 <jooyeon5204@gmail.com>
2024-07-21 21:07:00 +0900

    fix: Compare encrypted password in login method

--- src/main/java/com/example/project/LoginService.java
+++ src/main/java/com/example/project/LoginService.java
@@ -1,4 +1,4 @@
-package com.example.project.;
+package com.example.project.
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,36 +15,37 @@ public class LoginService {
     private final UserRepository repository;
 
     public User login(String id, String password) throws NoSuchAlgorithmException {
-        if(repository.existsById(id)){
+        if (repository.existsById(id)) {
             User user = repository.findById(id).get();
             String encryptedPassword = encryptPassword(password);// 비밀번호 암호화해서 저장된 비밀번호와 비교
-            if (user.getPassword().equals(password)){
+            if (user.getPassword().equals(encryptedPassword)) {
                 return user;
-            }else {
+            } else {
                 return new User(
                         user.getId(),
                         "error" //password가 일치하지 않음
                 );
             }
-        }else {
+        } else {
             return new User(
                     "error",    //ID가 일치하지 않음
                     null
             );
         }
     }
+
+
+    public String encryptPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes());
+        return bytesToHex(md.digest());
+    }// 입력받은 비밀번호 암호화하기
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes) {
+            builder.append(String.format("%02x", b));
+        }
+        return builder.toString();
+    }// 바이트 배열을 16진수 문자열로 변환
 }
-
-public String encryptPassword(String password) throws NoSuchAlgorithmException {
-    MessageDigest md = MessageDigest.getInstance("SHA-256");
-    md.update(password.getBytes());
-    return bytesToHex(md.digest());
-}// 입력받은 비밀번호 암호화하기
-
-private String bytesToHex(byte[] bytes) {
-    StringBuilder builder = new StringBuilder();
-    for (byte b : bytes) {
-        builder.append(String.format("%02x", b));
-    }
-    return builder.toString();
-}// 바이트 배열을 16진수 문자열로 변환
