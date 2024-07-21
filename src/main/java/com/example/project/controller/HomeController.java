0e03c2ce55f7e5553c38ef13340211b32e98d038
kyungmin lee <alsdl7950@gmail.com>
2024-07-22 02:43:07 +0900

    update:로그인전, 후 파일생성, 경로변경

--- src/main/java/com/example/project/controller/HomeController.java
+++ src/main/java/com/example/project/controller/HomeController.java
@@ -1,14 +1,7 @@
 package com.example.project.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -22,25 +15,19 @@ public class HomeController {
 
         //model.addAttribute("name","kim");
 
-        return "main_page";
+        return "beforelogin/main_page";
     }
 
-    @GetMapping("/signin_page")
-    public ModelAndView signinPage(@ModelAttribute Signin signin, HttpServletRequest request) throws  Exception
-    {
-       return new ModelAndView("signin_page");
+    @GetMapping("/login")
+    public String loginPage() {
+        return "beforelogin/login_page";
     }
 
-    @RequestMapping("/signin.do")
-    public ModelAndView doSignin(@Valid Signin signin, BindingResult result,
-           RedirectAttributes redirect, HttpServletRequest request, HttpServletResponse response)
-           throws Exception {
-        return new ModelAndView("main_page");
-    }
-    @GetMapping("/signup_page")
+    @GetMapping("/signup")
     public String signupPage() {
 
-        return "signup_page";
+        return "beforelogin/signup_page";
     }
+
 }
 
