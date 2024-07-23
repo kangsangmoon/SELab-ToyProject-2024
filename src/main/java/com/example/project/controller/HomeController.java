f801ab7f78922a11dfeebf82e886b04b7a54e608
kyungmin lee <alsdl7950@gmail.com>
2024-07-24 00:39:14 +0900

    update: 객체 던져주고 값 넘겨받기

--- src/main/java/com/example/project/controller/HomeController.java
+++ src/main/java/com/example/project/controller/HomeController.java
@@ -1,7 +1,17 @@
 package com.example.project.controller;
 
+
+import com.example.project.LoginForm;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -15,18 +25,25 @@ public class HomeController {
 
         //model.addAttribute("name","kim");
 
-        return "beforelogin/main_page";
+        return "login/before/main";
     }
 
     @GetMapping("/login")
-    public String loginPage() {
-        return "beforelogin/login_page";
+    public String loginPage(@ModelAttribute LoginForm loginForm, Model model, HttpServletRequest request)throws Exception  {
+
+        return "login/before/login";
+    }
+
+    @PostMapping("/login.do")
+    public String login(@Validated LoginForm loginForm, Model model, HttpServletRequest request, BindingResult result,
+                        RedirectAttributes redirect, HttpServletResponse response)throws Exception  {
+        return "login/aftre/signin_main";
     }
 
     @GetMapping("/signup")
     public String signupPage() {
 
-        return "beforelogin/signup_page";
+        return "login/before/signup";
     }
 
 }
