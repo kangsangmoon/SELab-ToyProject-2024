e9444200b30ad097e75559afcbdfe26e900ca357
kyungmin lee <alsdl7950@gmail.com>
2024-07-24 13:49:56 +0900

    update: 빈 객체를 만들어서 화면을 넘기도록 만들었습니다.

--- src/main/java/com/example/project/controller/HomeController.java
+++ src/main/java/com/example/project/controller/HomeController.java
@@ -2,16 +2,12 @@ package com.example.project.controller;
 
 
 import com.example.project.LoginForm;
+import com.example.project.SignupForm;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -24,27 +20,19 @@ public class HomeController {
         //thymeleaf
 
         //model.addAttribute("name","kim");
-
         return "login/before/main";
     }
 
     @GetMapping("/login")
-    public String loginPage(@ModelAttribute LoginForm loginForm, Model model, HttpServletRequest request)throws Exception  {
-
+    public String loginPage(Model model) {
+        model.addAttribute("LoginForm", new LoginForm());
         return "login/before/login";
     }
 
-    @PostMapping("/login.do")
-    public String login(@Validated LoginForm loginForm, Model model, HttpServletRequest request, BindingResult result,
-                        RedirectAttributes redirect, HttpServletResponse response)throws Exception  {
-        return "login/aftre/signin_main";
-    }
-
     @GetMapping("/signup")
-    public String signupPage() {
-
+    public String signupPage(Model model) {
+        model.addAttribute("SignupForm", new SignupForm());
         return "login/before/signup";
     }
-
 }
 
