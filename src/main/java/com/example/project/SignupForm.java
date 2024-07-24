297e63f9e60c7e5ab9a6d3e2746c82ad7c291db0
kyungmin lee <alsdl7950@gmail.com>
2024-07-24 13:51:08 +0900

    update: Signup 클래스와 필드 만들었습니다.

---
+++ src/main/java/com/example/project/SignupForm.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+
+import lombok.Data;
+
+@Data
+public class SignupForm {
+    private String userId;
+    private String password;
+    private String repeatPassword;
+    private String userName;
+    private String email;
+}
