116e480abb7d02087aef9c38e6a749bcce651786
kyungmin lee <alsdl7950@gmail.com>
2024-07-24 13:50:41 +0900

    delete: 객체 생성 코드 삭제 했습니다.

--- src/main/java/com/example/project/LoginForm.java
+++ src/main/java/com/example/project/LoginForm.java
@@ -5,11 +5,7 @@ import lombok.Data;
 
 @Data
 public class LoginForm {
-    public static void login(String[] args) {
-        LoginForm loginForm = new LoginForm();
-    }
 
     private String userId;
     private String password;
-
 }
