99fbd3100bcdde6536522e9695b5965bf411fedb
kangs <sangmoonkang@naver.com>
2024-07-27 00:26:46 +0900

    update userId, repeatPassword, userName 추가

--- src/main/java/com/example/project/dto/AddUserRequest.java
+++ src/main/java/com/example/project/dto/AddUserRequest.java
@@ -1,11 +1,12 @@
 package com.example.project.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class AddUserRequest {
-    private String email;
+    private String userId;
     private String password;
+    private String repeatPassword;
+    private String userName;
+    private String email;
 }
