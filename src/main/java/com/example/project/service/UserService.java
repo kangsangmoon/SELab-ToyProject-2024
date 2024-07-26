ca4a7044d435b739ef139198f63a1630b4f193c7
kangs <sangmoonkang@naver.com>
2024-07-27 00:28:41 +0900

    update 사용자 추가 기능

--- src/main/java/com/example/project/service/UserService.java
+++ src/main/java/com/example/project/service/UserService.java
@@ -3,22 +3,49 @@ package com.example.project.service;
 import com.example.project.dto.AddUserRequest;
 import com.example.project.entity.User;
 import com.example.project.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public Long save(AddUserRequest dto) {
-        return userRepository.save(User.builder()
-                .email(dto.getEmail())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
-                .build()).getId();
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void addUser(AddUserRequest addUserRequest) {
+        if (!addUserRequest.getPassword().equals(addUserRequest.getRepeatPassword())) {
+            throw new IllegalStateException("비밀번호가 일치하지 않습니다");
+        }
+
+        Optional<User> existingUser = userRepository.findByEmail(addUserRequest.getEmail());
+        if (existingUser.isPresent()) {
+            throw new IllegalStateException("이미 사용되고 있는 이메일입니다");
+        }
+
+        User user = new User(
+                addUserRequest.getUserId(),
+                bCryptPasswordEncoder.encode(addUserRequest.getPassword()),
+                addUserRequest.getUserName(),
+                addUserRequest.getEmail()
+        );
+        userRepository.save(user);
+    }
+
+    public boolean authenticate(String userId, String rawPassword) {
+        Optional<User> userOptional = userRepository.findByUserId(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            return bCryptPasswordEncoder.matches(rawPassword, user.getPassword());
+        }
+        return false;
     }
 }
-
