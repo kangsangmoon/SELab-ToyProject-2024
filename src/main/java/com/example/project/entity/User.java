44ea8a0ec0b903060ec4bc2694c7db953dafd21c
kangs <sangmoonkang@naver.com>
2024-07-27 00:23:02 +0900

    update userId, userName 추가 및 코드 제거

--- src/main/java/com/example/project/entity/User.java
+++ src/main/java/com/example/project/entity/User.java
@@ -1,62 +1,26 @@
 package com.example.project.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.List;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-public class User implements UserDetails {
+@NoArgsConstructor
+@Data
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String email;
+    private String userId;
     private String password;
+    private String userName;
+    private String email;
 
-    @Builder
-    public User(String email, String password) {
-        this.email = email;
+    public User(String userId, String password, String userName, String email) {
+        this.userId = userId;
         this.password = password;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
+        this.userName = userName;
+        this.email = email;
     }
 }
