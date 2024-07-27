4ef9d56ea8eba1ad17e054a6c4bfc286cbeb583d
usr <rlarlgus0206@naver.com>
2024-07-27 22:41:08 +0900

    update : logger

--- src/main/java/com/example/project/controller/HomeController.java
+++ src/main/java/com/example/project/controller/HomeController.java
@@ -30,6 +30,14 @@ public class HomeController {
         return "login/before/login";
     }
 
+    @PostMapping("/login")
+    public String login(LoginForm loginForm){
+        log.info("[ SYSTEM ] Login Tried ID : {}",loginForm.getUserId());
+        log.info("[ SYSTEM ] Login Tried PASSWORD : {}",loginForm.getPassword());
+
+        return null;
+    }
+
     @GetMapping("/signup")
     public String signupPage(Model model) {
         model.addAttribute("SignupForm", new SignupForm());
@@ -38,8 +46,8 @@ public class HomeController {
 
     @PostMapping("/signup")
     public String singup(SignupForm signupForm){
-        log.info(signupForm.getUserId());   //로그로 아이디를 띄움
-        log.info(signupForm.getPassword());
+        log.info("[ SYSTEM ] REGISTER Tried ID : {}",signupForm.getUserId());   //로그로 아이디를 띄움
+        log.info("[ SYSTEM ] REGISTER Tried PASSWORD : {}",signupForm.getPassword());
 
         return null;    //null 값을 받아왔기 때문에 화이트라벨 에러가 발생함
     }
