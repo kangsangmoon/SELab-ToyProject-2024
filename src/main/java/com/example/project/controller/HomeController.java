14fa356b3bab26b78f2836cac71e67fd4b35150e
usr <rlarlgus0206@naver.com>
2024-07-27 22:36:51 +0900

    update : signup method

--- src/main/java/com/example/project/controller/HomeController.java
+++ src/main/java/com/example/project/controller/HomeController.java
@@ -3,13 +3,14 @@ package com.example.project.controller;
 
 import com.example.project.LoginForm;
 import com.example.project.SignupForm;
-import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
+@Slf4j
 @Controller
 public class HomeController {
 
@@ -33,6 +34,14 @@ public class HomeController {
     public String signupPage(Model model) {
         model.addAttribute("SignupForm", new SignupForm());
         return "login/before/signup";
+    }
+
+    @PostMapping("/signup")
+    public String singup(SignupForm signupForm){
+        log.info(signupForm.getUserId());   //로그로 아이디를 띄움
+        log.info(signupForm.getPassword());
+
+        return null;    //null 값을 받아왔기 때문에 화이트라벨 에러가 발생함
     }
 }
 
