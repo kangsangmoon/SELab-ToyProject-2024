d926065640dd0b50538ca1ae04e4324504523852
kangs <sangmoonkang@naver.com>
2024-08-01 17:08:09 +0900

    update UserService

--- src/main/java/com/example/project/service/UserService.java
+++ src/main/java/com/example/project/service/UserService.java
@@ -4,50 +4,44 @@ import com.example.project.dto.AddUserRequest;
 import com.example.project.entity.User;
 import com.example.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 @Service
-//TODO LOMBOK 에노테이션 잘 사용하기
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    //TODO 컨트롤러에
     public void addUser(AddUserRequest addUserRequest) {
-        if (!addUserRequest.getPassword().equals(addUserRequest.getRepeatPassword())) {
+        if (!addUserRequest.getPassword().equals(addUserRequest.getConfirmPassword())) {
             throw new IllegalStateException("비밀번호가 일치하지 않습니다");
         }
 
-        //Optional<User> existingUser = userRepository.findByEmail(addUserRequest.getEmail());
         if (userRepository.existsByEmail(addUserRequest.getEmail())) {
             throw new IllegalStateException("이미 사용되고 있는 이메일입니다");
         }
 
-        User user = new User(
-                addUserRequest.getUserId(),
-                addUserRequest.getPassword(),
-                addUserRequest.getUserName(),
-                addUserRequest.getEmail()
-        );
+        //조건문 추가(입력한 이메일 인증번호가 일치하지 않을 때 exception 처리 일치할 경우 입력 정보 저장)
 
-        userRepository.save(user);
+        User user = new User();
+        user.setUserId(addUserRequest.getUserId());
+        user.setUserName(addUserRequest.getUserName());
+        user.setEmail(addUserRequest.getEmail());
+        user.setPassword(addUserRequest.getPassword()); // 비밀번호 암호화 제거
+
+        userRepository.save(user); // 사용자 저장
     }
 
     public boolean authenticate(String userId, String password) {
         Optional<User> userOptional = userRepository.findByUserId(userId);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
-            return bCryptPasswordEncoder.matches(password, user.getPassword());
+            return password.equals(user.getPassword());
         }
         return false;
     }
