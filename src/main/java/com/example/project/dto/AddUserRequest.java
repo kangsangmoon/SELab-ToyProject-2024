8db92ff9561817196ff6c46092408b194dfef1bc
kangs <sangmoonkang@naver.com>
2024-08-01 17:06:03 +0900

    update 회원가입 정보

--- src/main/java/com/example/project/dto/AddUserRequest.java
+++ src/main/java/com/example/project/dto/AddUserRequest.java
@@ -6,7 +6,8 @@ import lombok.Data;
 public class AddUserRequest {
     private String userId;
     private String password;
-    private String repeatPassword;
+    private String confirmPassword;
     private String userName;
     private String email;
+    private String confirmEmail;
 }
