c35e18d751294ff58eb2f5d71ab913e2ddef93b3
sangm <sangmoonkang@naver.com>
2024-08-08 23:08:53 +0900

    fix : 패스워드 암호화 후 비교

--- src/main/java/com/example/project/user/service/LoginService.java
+++ src/main/java/com/example/project/user/service/LoginService.java
@@ -8,6 +8,8 @@ import com.example.project.user.dto.UserResponse;
 import com.example.project.user.dto.login.LoginRequest;
 import com.example.project.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class LoginService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public UserResponse login(LoginRequest request){
@@ -24,8 +27,7 @@ public class LoginService {
                     throw new InvalidLoginUserIdException(ErrorMessage.INVALID_ID_TO_LOGIN, "ID로 유저를 찾을 수 없습니다");
                 }
         );
-
-        if(user.getPassword().equals(request.getPassword())){
+        if (passwordEncoder.matches(request.getPassword(), user.getPassword())){
             return user.toResponseDto();
         }else throw new InvalidLoginPasswordException(ErrorMessage.INVALID_PASSWORD_TO_LOGIN,"PASSWORD가 일치하지 않습니다");
     }
