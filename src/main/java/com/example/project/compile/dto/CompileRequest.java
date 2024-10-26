35c4f920a4fe7bae59204432e4c455c8eb1c9302
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-10-26 20:06:06 +0900

    update(Compile): 예제를 넘기는 구문 추가

--- src/main/java/com/example/project/compile/dto/CompileRequest.java
+++ src/main/java/com/example/project/compile/dto/CompileRequest.java
@@ -1,9 +1,18 @@
 package com.example.project.compile.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Data
 public class CompileRequest {
     private String language;
     private String code;
+
+    @Setter
+    @Getter
+    private List<String> examples;
+
 }
