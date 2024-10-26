a18745ae52fc1f33a2224dadc03b094f1ce73ab0
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-10-26 20:05:45 +0900

    update(Compile): 예제를 넘기는 구문 추가

--- src/main/java/com/example/project/compile/controller/rest/CompileApiController.java
+++ src/main/java/com/example/project/compile/controller/rest/CompileApiController.java
@@ -1,36 +1,46 @@
 package com.example.project.compile.controller.rest;
 
+import com.example.project.auth.service.AuthTokenService;
 import com.example.project.common.dto.ResponseDto;
 import com.example.project.common.dto.ResponseMessage;
-import com.example.project.solution.dto.request.user.SolutionCompileRequest;
+import com.example.project.common.util.HeaderUtil;
+import com.example.project.compile.dto.CompileRequest;
 import com.example.project.compile.service.CompileService;
+import com.example.project.error.dto.ErrorMessage;
+import com.example.project.error.dto.ErrorResponseDto;
+import com.example.project.example.service.ExampleService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/compile")
+@RequestMapping("/api")
 public class CompileApiController {
-    private final CompileService compileService;
 
-    @PostMapping
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<?> compileCode(@RequestBody SolutionCompileRequest request) {
-        try {
-            String result = compileService.compileAndRun(request.getLanguage(), request.getCode());
+    private final CompileService compileService;
+    private final AuthTokenService authTokenService;
+    private final ExampleService exampleService;
+
+    @PostMapping("/compile")
+    public ResponseEntity<?> compileCode(@RequestBody CompileRequest request, HttpServletRequest httpServletRequest) throws IOException {
+        if (authTokenService.isValidateToken(HeaderUtil.resolveToken(httpServletRequest))) {
+            List<Object> convertedExamples = exampleService.convertExamples(request.getExamples());
+            String result = compileService.compileAndRun(
+                    request.getLanguage(),
+                    request.getCode(),
+                    convertedExamples
+            );
             return ResponseDto.toResponseEntity(ResponseMessage.COMPILE_SUCCESS, result);
-        } catch (IllegalArgumentException e) {
-            return ResponseDto.toResponseEntity(ResponseMessage.INVALID_LANGUAGE, e.getMessage());
-        } catch (IOException e) {
-            return ResponseDto.toResponseEntity(ResponseMessage.IO_ERROR, e.getMessage());
-        } catch (InterruptedException e) {
-            return ResponseDto.toResponseEntity(ResponseMessage.EXECUTION_INTERRUPTED, e.getMessage());
-        } catch (Exception e) {
-            return ResponseDto.toResponseEntity(ResponseMessage.GENERAL_COMPILE_ERROR, e.getMessage());
-        }
+        } else return ErrorResponseDto.of(ErrorMessage.NOT_FOUND_CLIENT_ID_HEADER);
     }
 }
