9a3a096f51ead5b76f34b8e5db54a7d9789ba7fe
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-11-01 21:02:43 +0900

    update(Compile): 리스트가 아닌 예제ID로 예제를 받아온다

--- src/main/java/com/example/project/compile/dto/CompileRequest.java
+++ src/main/java/com/example/project/compile/dto/CompileRequest.java
@@ -1,18 +1,10 @@
 package com.example.project.compile.dto;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
 
 @Data
 public class CompileRequest {
     private String language;
     private String code;
-
-    @Setter
-    @Getter
-    private List<String> examples;
-
+    private Long exampleId;
 }
