861f3cf544e3212be90eb299ee4b76edc4e126e0
kangsangmoon <42689622+kangsangmoon@users.noreply.github.com>
2024-11-10 22:41:43 +0900

    update(Example): 사용하지 않는 findAllBySolutionId 메서드 삭제

--- src/main/java/com/example/project/example/repository/ExampleRepository.java
+++ src/main/java/com/example/project/example/repository/ExampleRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ExampleRepository  extends JpaRepository<Example, Long>, JpaSpecificationExecutor<Example> {
-    Optional<Example> findBySolutionId(Long solutionId, Long id);
-
-    List<Example> findAllBySolutionId(Long solutionId);
+    Optional<Example> findBySolutionId(Long solutionId);
 }
